a51bbbaf5ed3de0f970515e47c2562bdcba70f70
Bryan P. Galligan, SJ <bgalligan@jesuits.org>
2021-12-22 16:36:14 +0300

    Prepare data for ANOVA on catch composition (functional group by diet)

--- 02_Stability_FishTrapsFoodSec.R
+++ 02_Stability_FishTrapsFoodSec.R
@@ -24,6 +24,190 @@
 
 
 
+# First, clean the environment
+rm(list = ls())
+
+
+
+
+##### 2.1 Load Packages and Data #####
+
+# Load packages
+library(readr)
+library(tidyr)
+library(dplyr)
+
+# Load Trap Data
+TrapData <- read_csv("01_CleanData_Out/TrapData_Cleaned.csv")
+
+
+
+
+##### 2.2 FunGr_Diet #####
+
+## Hypothesis: Experimental traps contribute to reef resilience by catching fewer browsers,
+##  scrapers, and grazers.
+
+
+## Independent variables:
+##    TrapType (categorical, 2 levels)
+##    GateCode (categorical, 7 levels) *blocking variable
+##    Site (categorical, 20 levels)
+
+## Dependent variable:
+##    Functional group
+
+# Delete all rows with NA for FunGr_Diet
+TrapData_FunGrDiet <- TrapData[!is.na(TrapData$FunGr_Diet),]
+
+# Delete all rows with NA for GateCode
+TrapData_FunGrDiet <- TrapData_FunGrDiet[!is.na(TrapData_FunGrDiet$GateCode),]
+
+# Bin all non-browsers/scrapers/grazers into one functional group ("Other")
+for(i in 1:length(TrapData_FunGrDiet$FunGr_Diet)){
+  
+  if(TrapData_FunGrDiet$FunGr_Diet[i] == "Cleaner"){
+    TrapData_FunGrDiet$FunGr_Diet[i] <- "Other"
+  }
+  
+  if(TrapData_FunGrDiet$FunGr_Diet[i] == "Detritivore"){
+    TrapData_FunGrDiet$FunGr_Diet[i] <- "Other"
+  }
+  
+  if(TrapData_FunGrDiet$FunGr_Diet[i] == "Invert-Macro"){
+    TrapData_FunGrDiet$FunGr_Diet[i] <- "Other"
+  }
+  
+  if(TrapData_FunGrDiet$FunGr_Diet[i] == "Invert-Micro"){
+    TrapData_FunGrDiet$FunGr_Diet[i] <- "Other"
+  }
+  
+  if(TrapData_FunGrDiet$FunGr_Diet[i] == "Pisc-Macro-Invert"){
+    TrapData_FunGrDiet$FunGr_Diet[i] <- "Other"
+  }
+  
+  if(TrapData_FunGrDiet$FunGr_Diet[i] == "Piscivore"){
+    TrapData_FunGrDiet$FunGr_Diet[i] <- "Other"
+  }
+  
+  if(TrapData_FunGrDiet$FunGr_Diet[i] == "Planktivore"){
+    TrapData_FunGrDiet$FunGr_Diet[i] <- "Other"
+  }
+  
+  if(TrapData_FunGrDiet$FunGr_Diet[i] == "Predator"){
+    TrapData_FunGrDiet$FunGr_Diet[i] <- "Other"
+  }
+  
+  if(TrapData_FunGrDiet$FunGr_Diet[i] == "Variable"){
+    TrapData_FunGrDiet$FunGr_Diet[i] <- "Other"
+  }
+  
+}
+
+# Preliminary data frame of unique variable combinations for ANOVA
+GateCode <- unique(TrapData_FunGrDiet$GateCode)
+Site <- unique(TrapData_FunGrDiet$Site)
+AOV_FunGrDiet <- expand_grid(GateCode, Site)
+
+# Add columns to data AOV Data Frame
+AOV_FunGrDiet$TrapType <- NA
+AOV_FunGrDiet$BrowserCt <- NA
+AOV_FunGrDiet$BrowserMass_g <- NA
+AOV_FunGrDiet$BrowserCtRatio <- NA
+AOV_FunGrDiet$BrowserMassRatio <- NA
+AOV_FunGrDiet$GrazerCt <- NA
+AOV_FunGrDiet$GrazerMass_g <- NA
+AOV_FunGrDiet$GrazerCtRatio <- NA
+AOV_FunGrDiet$GrazerMassRatio <- NA
+AOV_FunGrDiet$ScraperCt <- NA
+AOV_FunGrDiet$ScraperMass_g <- NA
+AOV_FunGrDiet$ScraperCtRatio <- NA
+AOV_FunGrDiet$ScraperMassRatio <- NA
+AOV_FunGrDiet$OtherCt <- NA
+AOV_FunGrDiet$OtherMass_g <- NA
+AOV_FunGrDiet$OtherCtRatio <- NA
+AOV_FunGrDiet$OtherMassRatio <- NA
+AOV_FunGrDiet$KeyHerbivoreCt <- NA
+AOV_FunGrDiet$KeyHerbivoreMass_g <- NA
+AOV_FunGrDiet$KeyHerbivoreCtRatio <- NA
+AOV_FunGrDiet$KeyHerbivoreMassRatio <- NA
+AOV_FunGrDiet$TotalCt <- NA
+AOV_FunGrDiet$TotalMass_g <- NA
+
+# Fill in TrapType column of AOV data frame
+for (i in 1:length(AOV_FunGrDiet$TrapType)){
+  
+  if(AOV_FunGrDiet$GateCode[i] == "T"){
+    AOV_FunGrDiet$TrapType[i] <- "Traditional"
+  } else{
+    AOV_FunGrDiet$TrapType[i] <- "Gated"
+  }
+  
+}
+
+# Fill in count and mass columns of AOV Data Frame
+for (i in 1:length(AOV_FunGrDiet$GateCode)){
+  
+  # Subset data applicable to this row
+  a <- subset(TrapData_FunGrDiet, 
+    GateCode == AOV_FunGrDiet$GateCode[i] & Site == AOV_FunGrDiet$Site[i])
+  
+  # Calculate BrowserCt
+  AOV_FunGrDiet$BrowserCt[i] <- sum(a$FunGr_Diet == "Browser")
+  
+  # Calculate GrazerCt
+  AOV_FunGrDiet$GrazerCt[i] <- sum(a$FunGr_Diet == "Grazer")
+  
+  # Calculate ScraperCt
+  AOV_FunGrDiet$ScraperCt[i] <- sum(a$FunGr_Diet == "Scrapers/Excavators")
+  
+  # Calculate OtherCt
+  AOV_FunGrDiet$OtherCt[i] <- sum(a$FunGr_Diet == "Other")
+  
+  # Calculate TotalCt
+  AOV_FunGrDiet$TotalCt[i] <- length(a$FunGr_Diet)
+  
+  # Calculate BrowserMass_g
+  b <- subset(a, a$FunGr_Diet == "Browser")
+  AOV_FunGrDiet$BrowserMass_g[i] <- sum(b$Weight_g, na.rm = TRUE)
+  
+  # Calculate GrazerMass_g
+  b <- subset(a, a$FunGr_Diet == "Grazer")
+  AOV_FunGrDiet$GrazerMass_g[i] <- sum(b$Weight_g, na.rm = TRUE)
+  
+  # Calculate ScraperMass_g
+  b <- subset(a, a$FunGr_Diet == "Scrapers/Excavators")
+  AOV_FunGrDiet$ScraperMass_g[i] <- sum(b$Weight_g, na.rm = TRUE)
+  
+  # Calculate OtherMass_g
+  b <- subset(a, a$FunGr_Diet == "Other")
+  AOV_FunGrDiet$OtherMass_g[i] <- sum(b$Weight_g, na.rm = TRUE)
+  
+  # Calculate TotalMass
+  AOV_FunGrDiet$TotalMass_g[i] <- sum(a$Weight_g, na.rm = TRUE)
+  
+}
+
+# Fill in ratio columns of AOV data frame
+AOV_FunGrDiet$BrowserCtRatio <- AOV_FunGrDiet$BrowserCt / AOV_FunGrDiet$TotalCt
+AOV_FunGrDiet$BrowserMassRatio <- AOV_FunGrDiet$BrowserMass_g / AOV_FunGrDiet$TotalMass_g
+AOV_FunGrDiet$GrazerCtRatio <- AOV_FunGrDiet$GrazerCt / AOV_FunGrDiet$TotalCt
+AOV_FunGrDiet$GrazerMassRatio <- AOV_FunGrDiet$GrazerMass_g / AOV_FunGrDiet$TotalMass_g
+AOV_FunGrDiet$ScraperCtRatio <- AOV_FunGrDiet$ScraperCt / AOV_FunGrDiet$TotalCt
+AOV_FunGrDiet$ScraperMassRatio <- AOV_FunGrDiet$ScraperMass_g / AOV_FunGrDiet$TotalMass_g
+AOV_FunGrDiet$OtherCtRatio <- AOV_FunGrDiet$OtherCt / AOV_FunGrDiet$TotalCt
+AOV_FunGrDiet$OtherMassRatio <- AOV_FunGrDiet$OtherMass_g / AOV_FunGrDiet$TotalMass_g
+
+# Fill in Key Herbivore columns of AOV data frame
+AOV_FunGrDiet$KeyHerbivoreCt <- AOV_FunGrDiet$BrowserCt + AOV_FunGrDiet$GrazerCt + AOV_FunGrDiet$ScraperCt
+AOV_FunGrDiet$KeyHerbivoreMass_g <- AOV_FunGrDiet$BrowserMass_g + AOV_FunGrDiet$GrazerMass_g + AOV_FunGrDiet$ScraperMass_g
+AOV_FunGrDiet$KeyHerbivoreCtRatio <- AOV_FunGrDiet$KeyHerbivoreCt / AOV_FunGrDiet$TotalCt
+AOV_FunGrDiet$KeyHerbivoreMassRatio <- AOV_FunGrDiet$KeyHerbivoreMass_g / AOV_FunGrDiet$TotalMass_g
+
+# Remove rows of AOV data frame where mass = 0 g
+AOV_FunGrDiet <- subset(AOV_FunGrDiet, AOV_FunGrDiet$TotalMass_g > 0)
+
+
 
 
 
