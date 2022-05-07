ba5c2a42509df70bc5b9a04f5446243f03ed3671
Bryan P. Galligan, SJ <bgalligan@jesuits.org>
2022-05-07 10:38:47 +0300

    Edited plots for trap model predictions

--- 06_AdditionalAnalysis_FishTrapsFoodSec.R
+++ 06_AdditionalAnalysis_FishTrapsFoodSec.R
@@ -39,9 +39,10 @@
 library(ggplot2)
 library(readr)
 library(moments)
-library(glmmTMB)
 library(ggeffects)
 library(DHARMa)
+library(glmmTMB)
+library(ggpubr)
 
 # Load trip data
 TripData <- read_csv("04_DataExploration_Out/TripDataForAnalysis_GatedTraps_Galligan.csv")
@@ -135,9 +136,12 @@ write.csv(CatchStabilityTransformed, file = "06_AdditionalAnalysis_Out/CatchStab
 
 ##### 6.3 TrapType and Food #####
 
-trap.food.model <- glmmTMB(FoodDim1 ~ TrapType, #+ (1|Site),
-  data = PCAData,
-  family = gaussian())
+# Linear model
+trap.food.model <- lm(FoodDim1 ~ TrapType,
+  data = PCAData)
+
+# Model output
+summary(trap.food.model)
 
 # Simulate residuals
 simulateResiduals(trap.food.model, plot = TRUE)
@@ -154,24 +158,35 @@ ggplot(data = PCAData, mapping = aes(FoodDim1)) +
 prediction <- ggpredict(trap.food.model, terms = c("TrapType"))
 
 # Plot the prediction
-ggplot(prediction, aes(y = predicted, x = x)) +
+plot.1a <- ggplot(prediction, aes(y = predicted, x = x)) +
   geom_point() +
-  geom_errorbar(aes(ymin = conf.low, ymax = conf.high, width = 0.2)) +
-  labs(title = "", x = "Trap Type", y = "Food Security") +
-  theme_bw() 
-  #annotate(geom = "text", x = "Traditional", y = 0.7, label = "p < 0.000")
+  geom_errorbar(aes(ymin = conf.low, ymax = conf.high, width = 0.1)) +
+  labs(title = "", x = "", y = "") +
+  scale_y_continuous(breaks = c(-0.34, -0.2, 0, 0.2, 0.3, 0.34),
+    labels = c("Calcium Concentration", -0.2, 0.0, 0.2, "Value Per Unit Effort", "Catch Per Unit Effort")) +
+  annotate(geom = "text", x = "Traditional", y = 0.25,
+    label = expression("p = 4.92 x 10"^-5)) +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+    axis.line = element_line(colour = "black"),
+    axis.ticks = element_blank()) +
+  coord_cartesian(ylim = c(-0.35, 0.35))
 
 # Gated traps have higher CPUE and value PUE, but lower Ca concentrations
 
 # Save the plot
-ggsave(filename = "06_AdditionalAnalysis_Out/TrapFoodPrediction.jpeg", device = "jpeg")
+# ggsave(filename = "06_AdditionalAnalysis_Out/TrapFoodPrediction.jpeg", device = "jpeg",
+#   height = 5, width = 3, units = "in")
 
 
 ##### 6.4 TrapType and Conservation 1 #####
 
-trap.cons1.model <- glmmTMB(ConsDim1 ~ TrapType,# + (1|Site),
-  data = PCAData,
-  family = gaussian())
+# Linear model
+trap.cons1.model <- lm(ConsDim1 ~ TrapType,
+  data = PCAData)
+
+# Model summary
+summary(trap.cons1.model)
 
 # Simulate residuals
 simulateResiduals(trap.cons1.model, plot = TRUE)
@@ -182,32 +197,42 @@ simulateResiduals(trap.cons1.model, plot = TRUE)
 ggplot(data = PCAData, mapping = aes(ConsDim1)) +
   geom_density()
 
-# This is not look a normal distribution...
+# This is not a normal distribution...
 
 # Get model predictions
 prediction <- ggpredict(trap.cons1.model, terms = c("TrapType"))
 
 # Plot the prediction
-ggplot(prediction, aes(y = predicted, x = x)) +
+plot.1b <- ggplot(prediction, aes(y = predicted, x = x)) +
   geom_point() +
-  geom_errorbar(aes(ymin = conf.low, ymax = conf.high, width = 0.2)) +
-  labs(title = "", x = "Trap Type", y = "Conservation Dim. 1") +
-  theme_bw()
-  #annotate(geom = "text", x = "Gated", y = 0.7, label = "p = 0.003")
+  geom_errorbar(aes(ymin = conf.low, ymax = conf.high, width = 0.1)) +
+  labs(title = "", x = "", y = "") +
+  scale_y_continuous(breaks = c(-0.34, -0.2, 0, 0.2, 0.34),
+    labels = c("Browsing Herbivores", -0.2, 0.0, 0.2, "Mean Trophic Level")) +
+  annotate(geom = "text", x = "Gated", y = 0.25,
+    label = expression("   p = 9.81 x 10"^-13)) +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+    axis.line = element_line(colour = "black"),
+    axis.ticks = element_blank()) +
+  coord_cartesian(ylim = c(-0.35, 0.35))
 
 # Traditional traps have higher mean trophic level and lower browser mass ratio
 
 # Save the plot
-ggsave(filename = "06_AdditionalAnalysis_Out/TrapCons1Prediction.jpeg", device = "jpeg")
+# ggsave(filename = "06_AdditionalAnalysis_Out/TrapCons1Prediction.jpeg", device = "jpeg")
 
 
 
 
 ##### 6.5 TrapType and Conservation 2 #####
 
-trap.cons2.model <- glmmTMB(ConsDim2 ~ TrapType,# + (1|Site),
-  data = PCAData,
-  family = gaussian())
+# Linear Model
+trap.cons2.model <- lm(ConsDim2 ~ TrapType,
+  data = PCAData)
+
+# Model summary
+summary(trap.cons2.model)
 
 # Simulate residuals
 simulateResiduals(trap.cons2.model, plot = TRUE)
@@ -224,22 +249,36 @@ ggplot(data = PCAData, mapping = aes(ConsDim2)) +
 prediction <- ggpredict(trap.cons2.model, terms = c("TrapType"))
 
 # Plot the prediction
-ggplot(prediction, aes(y = predicted, x = x)) +
+plot.1c <- ggplot(prediction, aes(y = predicted, x = x)) +
   geom_point() +
-  geom_errorbar(aes(ymin = conf.low, ymax = conf.high, width = 0.2)) +
-  labs(title = "", x = "Trap Type", y = "Conservation Dim. 2") +
-  theme_bw() +
-  annotate(geom = "text", x = "Traditional", y = 0.3, label = "p < 0.000")
+  geom_errorbar(aes(ymin = conf.low, ymax = conf.high, width = 0.1)) +
+  labs(title = "", x = "", y = "") +
+  scale_y_continuous(breaks = c(-0.34, -0.2, 0, 0.2, 0.27, 0.34),
+    labels = c("", -0.2, 0.0, 0.2, "Scraping Herbivores", expression(paste("Mean ", frac(L,L[mat]), sep = "")))) +
+  annotate(geom = "text", x = "Traditional", y = 0.25,
+    label = expression("p = 2.92 x 10"^-4)) +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+    axis.line = element_line(colour = "black"),
+    axis.ticks.y = element_blank()) +
+  coord_cartesian(ylim = c(-0.35, 0.35))
 
+# Gated traps have a higher mean L / Lmat and higher Scraper Mass Ratio
+  
 # Save the plot
-ggsave(filename = "06_AdditionalAnalysis_Out/TrapCons2Prediction.jpeg", device = "jpeg")
+# ggsave(filename = "06_AdditionalAnalysis_Out/TrapCons2Prediction.jpeg", device = "jpeg")
 
+# Plot 1
+ggarrange(plot.1a, plot.1b, plot.1c, nrow = 1)
 
-
+# Save Plot 1
+ggsave(filename = "06_AdditionalAnalysis_Out/TrapModelPredictions.jpeg", device = "jpeg",
+  height = 4, width = 12, units = "in")
 
 ##### 6.6 Food and Conservation 1 #####
 
-food.cons1.model <- glmmTMB(ConsDim1 ~ FoodDim1 + TrapType + (FoodDim1/TrapType) + (1|Site),
+# Linear Model
+food.cons1.model <- glmmTMB(ConsDim1 ~ FoodDim1 * TrapType,
   data = PCAData,
   family = gaussian())
 
@@ -255,9 +294,14 @@ prediction <- ggpredict(food.cons1.model, terms = c("FoodDim1"))
 ggplot(prediction, aes(y = predicted, x = x)) +
   geom_line() +
   geom_ribbon(aes(ymin = conf.low, ymax = conf.high), alpha = 0.2) +
+  geom_point(PCAData, mapping = aes(x = FoodDim1, y = ConsDim1,
+    color = TrapType), alpha = 0.3) +
+  coord_cartesian(ylim = c(-2, 2)) +
   labs(title = "", x = "Food Security", y = "Conservation Dim. 1") +
-  theme_bw() +
-  annotate(geom = "text", x = 4, y = 0.65, label = "p = 0.418")
+  annotate(geom = "text", x = -5, y = 1, label = "p = 0.418") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+    axis.line = element_line(colour = "black"))
 
 # Save the plot
 ggsave(filename = "06_AdditionalAnalysis_Out/FoodCons1Prediction.jpeg", device = "jpeg")
@@ -267,7 +311,7 @@ ggsave(filename = "06_AdditionalAnalysis_Out/FoodCons1Prediction.jpeg", device =
 
 ##### 6.7 Food and Conservation 2 #####
 
-food.cons2.model <- glmmTMB(ConsDim2 ~ FoodDim1 + TrapType + (FoodDim1/TrapType) + (1|Site),
+food.cons2.model <- glmmTMB(ConsDim2 ~ FoodDim1*TrapType,
   data = PCAData,
   family = gaussian())
 
@@ -282,11 +326,15 @@ prediction <- ggpredict(food.cons2.model, terms = c("FoodDim1"))
 # Plot the prediction
 ggplot(prediction, aes(y = predicted, x = x)) +
   geom_line() +
-  geom_point(mapping = aes(x = FoodDim1, y = ConsDim2), data = PCAData, alpha = 0.1) +
+  geom_point(mapping = aes(x = FoodDim1, y = ConsDim2,
+    color = TrapType), data = PCAData, alpha = 0.3) +
   geom_ribbon(aes(ymin = conf.low, ymax = conf.high), alpha = 0.2) +
+  coord_cartesian(ylim = c(-2.5, 2.5)) +
   labs(title = "", x = "Food Security", y = "Conservation Dim. 2") +
-  theme_bw() +
-  annotate(geom = "text", x = 4, y = 7, label = "p = 0.089")
+  annotate(geom = "text", x = -5, y = 0.7, label = "p = 0.089") +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+    axis.line = element_line(colour = "black"))
 
 # Save the plot
 ggsave(filename = "06_AdditionalAnalysis_Out/FoodCons1Prediction.jpeg", device = "jpeg")
